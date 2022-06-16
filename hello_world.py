1584072c5ecffc4548fa27322eed7d5fd38f3370
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-06-15 21:50:06 -0700

    Print, math, comments - day 1

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# message = 'Helloooo Python World!'
+# print(message)
+
+# message = "Hello from crash course!"
+# print(message)
+
+# first_name = "ada"
+# last_name = 'lovelace'
+# full_name = f"{first_name} {last_name}"
+# message = f"Hello, {full_name.title()}!"
+# print(message)
+
+# print("\tPython")
+
+# message = "One of Python's strengths is its diverse community"
+# print(message)
+
+# print(2+2)
+
+import this
