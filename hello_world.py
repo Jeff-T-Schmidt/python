aad1a2f2f66d553ecdd6049df6d594b8ad1aaad9
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-06-20 13:44:56 -0700

    Slice, tuples and lists

--- hello_world.py
+++ hello_world.py
@@ -17,4 +17,69 @@
 
 # print(2+2)
 
-import this
+# import this
+
+
+# names = ['nate', 'jeff', 'igor', 'josh']
+
+# print(names[0].title())
+# message = f"{names[0].title()} is lame!"
+# print(message)
+# names.append('wyland')
+# print(names)
+# names.insert(0, 'brady')
+# print(names)
+# del names[1]
+# print(names)
+
+# popped_names = names.pop(0)
+# print(names)
+# print(popped_names)
+
+# bad_magic = 'igor'
+# names.remove(bad_magic)
+# print(names)
+# print(f"\n {bad_magic.title()} is bad at magic!")
+
+# names.insert(0, 'zebra')
+# names.sort()
+# print(names)
+# print(len(names))
+
+# tosh_friends = ['liam', 'jesse', 'niko', 'star', 'brenda', 'jeff']
+
+# for tosh_friend in tosh_friends:
+#     print(f"{tosh_friend.title()} is Tosh's friend!")
+#     print(f"Tosh should go and stay at your house, if that is cool {tosh_friend.title()}.\n")
+
+# squares = []
+
+# for value in range(1,11):
+#     # square = value **2
+#     squares.append(value**2)
+# print(squares)
+
+# squares = [value**2 for value in range(1,11)]
+# print(squares)
+
+# my_foods = ['ice cream', 'chocolate cake', 'candy']
+# wife_foods = my_foods[:]
+
+# my_foods.append('cheesecake')
+# wife_foods.append('korean food')
+
+# print('My fav foods are:')
+# for my_food in my_foods:
+#     print(my_food)
+
+# print("\nMy wife's fav foods are:")
+# for wife_food in wife_foods:
+#     print(wife_food)
+
+# foods = ("veg","rice", "beans", "bread", "meat")
+# for food in foods:
+# # food[0] = 'water'
+#     print(food)
+# foods = ("\nveg","rice", "beans", "stuff", "water")
+# for food in foods:
+#     print(food)
